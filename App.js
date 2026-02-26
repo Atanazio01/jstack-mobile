6c6957d4029deb44e6c67275225161cad77f54f7
Atanazio01 <marcosd.atanazio@gmail.com>
2026-02-26 15:55:31 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text style={{
+        marginTop: 300,
+        fontSize: 32,
+        fontWeight: 'bold'
+      }}>
+        Hello JStack!
+      </Text>
+    </View>
+  );
+}
