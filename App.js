48f27265b56e7b13ba746d78676d604299f248b2
Atanazio01 <marcosd.atanazio@gmail.com>
2026-02-26 17:43:25 -0300

    controlling the number of lines in the Text component

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View>
-      <Text 
-      style={{ marginTop: 300, fontSize: 32, }}
-      selectable // -> Selecionável booleano para permitir que o texto seja selecionado
-      selectionColor="rgba(255,0,0,0.5)" // -> Cor da seleção do texto // * Android only
+      <Text
+        style={{ marginTop: 300, fontSize: 32 }}
+        //selectable // -> Selecionável booleano para permitir que o texto seja selecionado
+        //selectionColor="rgba(255,0,0,0.5)" // -> Cor da seleção do texto // * Android only
+        numberOfLines={1}
+        ellipsizeMode="tail" // -> Define como o texto é truncado quando ultrapassa o número de linhas definido
       >
-        Oi JStack!!!!
+        JStack - O melhor curso de React e JavaScript e a parada toda
       </Text>
     </View>
   );
