6f8eaf3711d765f0891032294a8140d9ea46188e
Atanazio01 <marcosd.atanazio@gmail.com>
2026-02-26 16:35:06 -0300

    properties: selectable and selectionColor

--- App.js
+++ App.js
@@ -3,13 +3,12 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View>
-      <Text style={{
-        marginTop: 300,
-        fontSize: 32,
-      }}>
+      <Text 
+      style={{ marginTop: 300, fontSize: 32, }}
+      selectable // -> Selecionável booleano para permitir que o texto seja selecionado
+      selectionColor="rgba(255,0,0,0.5)" // -> Cor da seleção do texto // * Android only
+      >
         Oi JStack!!!!
-
-        <Text style={{ fontWeight: 'bold' }}>Tudo bem?</Text>
       </Text>
     </View>
   );
