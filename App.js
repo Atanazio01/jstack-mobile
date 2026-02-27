b75491aec8678c344d4332c91726ce279e85ce5b
Atanazio01 <marcosd.atanazio@gmail.com>
2026-02-27 11:32:44 -0300

    controlling the scroll indicator / scroll bar

--- App.js
+++ App.js
@@ -3,25 +3,58 @@ import { Text, ScrollView } from 'react-native';
 export default function App() {
   return (
     <ScrollView
-      alwaysBounceVertical={false} // Só funciona no iOS, por padrão é true na ScrollView vertical e false na horizontal. Serve para desabilitar o efeito de "bounce" quando chegar no final da lista
+      persistentScrollbar // displays the scrollbar even when not scrolling, default value is false, only works on Android
+      indicatorStyle='white' // changes the color of the scrollbar, only works on iOS
+      showsVerticalScrollIndicator={false} // hides the vertical scrollbar, default value is true
     >
-      <Text
-        style={{marginTop: 300, fontSize:32}}
-      >
-        JStack - O melhor curso de React e JavaScript e a parada toda que existe
-      </Text>
-
-      <ScrollView 
+      <ScrollView
         horizontal
-        alwaysBounceHorizontal={false} // Só funciona no iOS, pois o android não tem esse efeito, por padrão é true na ScrollView horizontal e false na vertical. Serve para desabilitar o efeito de "bounce" quando chegar no final da lista
-        bounces={false} // Serve para desabilitar o efeito de "bounce" tanto no iOS quanto no Android, independente da orientação da ScrollView. Se for usado, ele irá sobrepor as propriedades alwaysBounceVertical e alwaysBounceHorizontal
+        style={{marginTop: 150}}
+        persistentScrollbar
+        showsHorizontalScrollIndicator={false}
       >
-        <Text
-          style={{fontSize:32}}
-        >
+        <Text style={{ fontSize: 32 }}>
           JStack - O melhor curso de React e JavaScript e a parada toda que existe
         </Text>
       </ScrollView>
+
+      <Text style={{ marginTop: 50, fontSize: 32 }}>
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+        JStack - O melhor curso de React e JavaScript e a parada toda que existe
+      </Text>
+
+
     </ScrollView>
   );
 }
