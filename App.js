f54b9c3f2046568158f32ed4d1d1a146ecec9c7b
Atanazio01 <marcosd.atanazio@gmail.com>
2026-02-27 14:37:59 -0300

    identify the platform where the code is running

--- App.js
+++ App.js
@@ -1,23 +1,21 @@
 import { Text, ScrollView, Platform } from 'react-native';
 
-Platform.OS // Operational System (iOS, Android, Web, etc.)
-Platform.Version // Version of the operational system
-Platform.constants // Constants of the operational system
-Platform.isPad // If the operational system is iPad
-Platform.isTV // If the operational system is TV
-Platform.isTesting // If the operational system is testing
-Platform.select({ // Select a value based on the operational system
-  ios: 'iOS',
-  android: 'Android',
-  default: 'Web'
-})
+const isAndroid = Platform.OS === 'android';
 
 export default function App() {
   return (
-
     <ScrollView>
-      <Text style={{ fontSize: 32, marginTop: 50 }}>
-        Hello JStack
+      <Text style={{ fontSize: 32, marginTop: isAndroid ? 50 : 100 }}>
+        {Platform.select({
+          ios: 'Hello, iOS!',
+          android: <Text style={{ color: 'green', fontWeight: 'bold' }}>Hello, Android!</Text>,
+          native: 'Hello, native platform!',
+          default: 'Hello, unknown platform!',
+        })}
+      </Text>
+      <Text style={{ fontSize: 24, marginTop: 20 }}>
+
+        {isAndroid ? 'Running on Android' : 'Running on iOS'}
       </Text>
     </ScrollView>
   );
