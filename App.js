66c5a89f2a15940237f2f6a6f6232c367107315d
Atanazio01 <marcosd.atanazio@gmail.com>
2026-02-27 19:02:07 -0300

    manipulating the StatusBar

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
-import { Text, ScrollView, Platform } from 'react-native';
-import { print } from './src/utils/print';
+import { ScrollView, StatusBar } from 'react-native';
 
 import { Button } from './src/Button';
 
-print();
-
 export default function App() {
   return (
-    <ScrollView style={{ marginTop: 200 }}>
+    <ScrollView style={{ backgroundColor: '#222' }}>
+      <StatusBar
+        hidden={false} //is supported on both iOS and Android, but behavior may vary. iOS hides StatusBar completely, Android may not support full hiding on all versions. Test on both platforms.
+        backgroundColor="#f00" //// Android: backgroundColormay be ignored on newer versions (Android 15+/API 35). Use native theming instead.
+        barStyle="light-content" // Supported on both platforms, but may not work on all Android versions. On iOS, it changes the text/icons to light color. On Android, behavior can be inconsistent across versions.
+        animated={false} // Disable animation on Android to avoid inconsistent color transition behavior on newer versions.
+        translucent={true} // Android: Setting to false ensures StatusBar does not overlay content, providing consistent layout across platforms. On iOS, this prop has no effect as StatusBar is non-translucent by default.
+      />
+
+      <Button />
+      <Button />
+      <Button />
       <Button />
     </ScrollView>
   );
