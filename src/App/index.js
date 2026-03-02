d61bc53ee5a3c28428246686e47151d041af039d
Atanazio01 <marcosd.atanazio@gmail.com>
2026-03-02 14:49:32 -0300

    separating UI styling

--- src/App/index.js
+++ src/App/index.js
@@ -1,15 +1,16 @@
 import { Platform, ScrollView, StatusBar, View } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
-import { Button } from './src/Button';
+import { Button } from '../Button';
+import { styles } from './styles';
 
 console.log(Platform.OS, StatusBar.currentHeight); // Android: Logs the height of the status bar. iOS: Logs undefined, as StatusBar height is not directly accessible.
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }} edges={['top', 'bottom']}>
-        <ScrollView style={{ backgroundColor: '#222' }}>
+      <SafeAreaView style={styles.wrapper} edges={['top', 'bottom']}>
+        <ScrollView style={styles.container}>
           <StatusBar
             hidden={false} //is supported on both iOS and Android, but behavior may vary. iOS hides StatusBar completely, Android may not support full hiding on all versions. Test on both platforms.
             backgroundColor="#f00" //// Android: backgroundColormay be ignored on newer versions (Android 15+/API 35). Use native theming instead.
@@ -18,7 +19,7 @@ export default function App() {
             translucent={true} // Android: Setting to false ensures StatusBar does not overlay content, providing consistent layout across platforms. On iOS, this prop has no effect as StatusBar is non-translucent by default.
           />
 
-          <View style={{ gap: 16 }}> 
+          <View style={styles.buttonsContainer}>
             <Button />
             <Button />
             <Button />
---
+++ src/App/styles.js
@@ -0,0 +1,14 @@
+import { Platform, StatusBar } from 'react-native';
+
+export const styles = {
+  wrapper: {
+    flex: 1,
+  },
+  container: {
+    backgroundColor: '#222',
+    height: '100%',
+  },
+  buttonsContainer: {
+    gap: 16,
+  }
+}
