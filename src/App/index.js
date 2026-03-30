0f7af89e4337f5568d16ce4b8ee803d8541910a8
Atanazio01 <marcosd.atanazio@gmail.com>
2026-03-30 10:53:04 -0300

    TouchableHighlight and TouchableWithoutFeedback components

--- src/App/index.js
+++ src/App/index.js
@@ -4,8 +4,10 @@ import {
   StatusBar,
   View,
   Button,
-  TouchableOpacity,
   Text,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
 } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
@@ -32,6 +34,23 @@ export default function App() {
             >
               <Text style={styles.buttonLabel}>TouchableOpacity</Text>
             </TouchableOpacity>
+            <TouchableHighlight
+              onPress={() => alert('TouchableHighlight pressionado')}
+              style={styles.button}
+              underlayColor="#f00"
+              activeOpacity={0.3} // só aplica opacidade ao conteúdo, não ao fundo, diferente do TouchableOpacity
+            >
+              <Text style={styles.buttonLabel}>TouchableHighlight</Text>
+              {/* Não é possível passar mais de um filho para o TouchableHighlight, diferente do TouchableOpacity, que aceita múltiplos filhos. Mas da para contornar isso usando um View como container ou um fragment. Sempre use uma View, pois o  activeOpacity por exemplo só sera aplicado no filho direto do TouchableHighlight e como o fragment não é um elemento real, ele não será afetado */}
+            </TouchableHighlight>
+            <TouchableWithoutFeedback
+              onPress={() => alert('TouchableWithoutFeedback pressionado')}
+              // O TouchableWithoutFeedback não tem feedback visual, ele não possui a propriedade styles, mas podemos utilizar um View como filho para aplicar estilos e criar um feedback visual personalizado, como mudar a cor de fundo ou adicionar uma animação. Uma dica do Mateus Silva é evitar de usar o TouchableWithoutFeedback, pois ele não tem feedback visual, o que pode ser confuso para os usuários, e é melhor usar o TouchableOpacity ou TouchableHighlight para fornecer um feedback visual claro. Assim como o TouchableHighlight, o TouchableWithoutFeedback também não permite múltiplos filhos diretos.
+            >
+              <View style={styles.button}>
+                <Text style={styles.buttonLabel}>TouchableWithoutFeedback</Text>
+              </View>
+            </TouchableWithoutFeedback>
           </View>
         </ScrollView>
       </SafeAreaView>
