4e60b9ca4dd26ac80271c6196fa041959b8384dc
Atanazio01 <marcosd.atanazio@gmail.com>
2026-04-27 16:30:28 -0300

    Navigating between Inputs using the return button

--- src/App/index.js
+++ src/App/index.js
@@ -4,10 +4,12 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { styles } from "./styles";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+import { useRef } from "react";
 
 console.log(Platform.OS, StatusBar.currentHeight); // Android: Logs the height of the status bar. iOS: Logs undefined, as StatusBar height is not directly accessible.
 
 export default function App() {
+  const passwordInputRef = useRef();
   const disabled = false;
 
   return (
@@ -20,13 +22,20 @@ export default function App() {
             autoCapitalize="none"
             autoCorrect={false}
             autoComplete="email"
+            onSubmitEditing={() => passwordInputRef.current.focus()}
+            submitBehavior="blurAndSubmit"
+            returnKeyType="next"
+            enablesReturnKeyAutomatically //* IOS Only
           />
           <Input
+            ref={passwordInputRef}
             placeholder="Senha"
             secureTextEntry
             keyboardType="number-pad"
+            returnKeyType="done"
+            onSubmitEditing={() => console.log("Enviar")}
           />
-          <Button disabled={disabled}>Oi Botão</Button>
+          <Button disabled={disabled}>Enviar</Button>
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,18 +1,19 @@
 import { TextInput } from "react-native";
 import { styles } from "./styles";
-import { useState } from "react";
+import { useEffect, useRef, useState, forwardRef } from "react";
 
 /**
  *
- * @param {import('react-native').TextInputProps} props
+ * @type {React.FC<import('react-native').TextInputProps>}
  */
 
-export function Input(props) {
+export const Input = forwardRef((props, ref) => {
   const [isFocused, setIsFocused] = useState(false);
 
   return (
     <>
       <TextInput
+        ref={ref}
         style={[styles.input, isFocused && styles.inputFocused]}
         placeholderTextColor="#aaa"
         onFocus={() => setIsFocused(true)}
@@ -21,4 +22,4 @@ export function Input(props) {
       />
     </>
   );
-}
+});
