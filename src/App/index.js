dc55c0940617aa6606c06518250dc236e8d281aa
Atanazio01 <marcosd.atanazio@gmail.com>
2026-05-19 11:13:32 -0300

    Solving the resizeMode cover + borderRadius bug on Android

--- src/App/index.js
+++ src/App/index.js
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { Image, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { Button } from '../components/Button';
+import { Button } from "../components/Button";
 import { styles } from "./styles";
 
 export default function App() {
@@ -14,13 +14,22 @@ export default function App() {
             source={{
               uri: "https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?q=80&w=1583&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             }}
-            style={{ width: 300, height: 150, backgroundColor: "red" }}
+            style={{
+              width: 300,
+              height: 150,
+              backgroundColor: "purple",
+              borderRadius: 20,
+              overlayColor: 'purple' // androir only // Na aula o Mateus fala que seria para resolver um bug no android onde ele pegava o ultimo pixel da imagem e esticava ele para o lado, mas aparentemente esse bug foi resolvido na versao 0.84.0 do react native, então essa propriedade não é mais necessária para resolver esse bug.
+            }}
             alt="Carro na garagem"
-            resizeMode="cover"
+            resizeMode="contain"
             blurRadius={isBlurred ? 30 : 0} // para adicionar blur na imagem
           />
 
-          <Button title="Adicionar blur" onPress={() => setIsBlurred(!isBlurred)}>
+          <Button
+            title="Adicionar blur"
+            onPress={() => setIsBlurred(!isBlurred)}
+          >
             {isBlurred ? "Mostrar imagem" : "Borrar imagem"}
           </Button>
         </View>
