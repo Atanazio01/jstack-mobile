3a48a63a0801ad89e05fc4dfaf6d417b696781fc
Atanazio01 <marcosd.atanazio@gmail.com>
2026-05-19 11:03:09 -0300

    Working with resizeMode and image styles

--- src/App/index.js
+++ src/App/index.js
@@ -1,24 +1,28 @@
+import { useState } from 'react';
 import { Image, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { Button } from '../components/Button';
 import { styles } from "./styles";
 
 export default function App() {
+  const [isBlurred, setIsBlurred] = useState(true);
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.wrapper}>
         <View style={styles.container}>
           <Image
             source={{
-              uri: "https://images.unsplash.com/photo-1580273916550-e323be2ae537?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-              width: 300,
-              height: 150,
-              method: 'GET', // caso precise de um metodo diferente de GET, como POST, PUT, DELETE, etc.
-              body: 'image/jpeg', // caso precise de um body, como POST, PUT, DELETE, etc.
-              headers: {
-                'Content-Type': 'image/jpeg', // caso precise de um header, como POST, PUT, DELETE, etc.
-              },
+              uri: "https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?q=80&w=1583&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             }}
+            style={{ width: 300, height: 150, backgroundColor: "red" }}
+            alt="Carro na garagem"
+            resizeMode="cover"
+            blurRadius={isBlurred ? 30 : 0} // para adicionar blur na imagem
           />
+
+          <Button title="Adicionar blur" onPress={() => setIsBlurred(!isBlurred)}>
+            {isBlurred ? "Mostrar imagem" : "Borrar imagem"}
+          </Button>
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
