a3f840f17bfcd7d6c3a7cc69a3dbae60e91f2a14
Atanazio01 <marcosd.atanazio@gmail.com>
2026-05-21 10:45:05 -0300

    Capturing screen dimensions and detecting portrait and landscape modes.

--- src/App/index.js
+++ src/App/index.js
@@ -1,30 +1,24 @@
-import { useState } from "react";
-import { ImageBackground, View } from "react-native";
+import { Text, useWindowDimensions, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { Button } from "../components/Button";
 import { styles } from "./styles";
 
 export default function App() {
-  const [isBlurred, setIsBlurred] = useState(true);
+  const { fontScale, scale, width, height } = useWindowDimensions();
+
+  const orientation = width > height ? "landscape" : "portrait";
 
   return (
     <SafeAreaProvider>
-      <ImageBackground source={require("../images/avatarIA.jpg")} style={{ flex: 1 }}>
-        
-
-        <SafeAreaView style={styles.wrapper}>
-          <View style={styles.container}>
-
-
-            <Button
-              title="Adicionar blur"
-              onPress={() => setIsBlurred(!isBlurred)}
-            >
-              {isBlurred ? "Mostrar imagem" : "Borrar imagem"}
-            </Button>
-          </View>
-        </SafeAreaView>
-      </ImageBackground>
+      <SafeAreaView style={styles.wrapper}>
+        <View style={styles.container}>
+          <Text>Font Scale: {fontScale}</Text>
+          <Text>Pixel Ratio: {scale}</Text>
+          <Text>
+            Dimensions: {width}x{height}
+          </Text>
+          <Text>Orientation: {orientation}</Text>
+        </View>
+      </SafeAreaView>
     </SafeAreaProvider>
   );
 }
