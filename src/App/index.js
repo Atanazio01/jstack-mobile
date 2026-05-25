d12c9e824261983d78a29e48e0a166359ec1d1d4
Atanazio01 <marcosd.atanazio@gmail.com>
2026-05-25 10:38:37 -0300

    InputAccessoryView component

--- src/App/index.js
+++ src/App/index.js
@@ -1,47 +1,28 @@
-import { DrawerLayoutAndroid, Text, View } from "react-native";
+import { InputAccessoryView, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { styles } from "./styles";
 
-import { useRef } from "react";
 import { Button } from "../components/Button";
+import { Input } from "../components/Input";
 
 export default function App() {
-  const drawerRef = useRef(null);
-
-  function handleOpenMenu() {
-    drawerRef.current.openDrawer();
-  }
-
-  function handleCloseMenu() {
-    drawerRef.current.closeDrawer();
-  }
-
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.wrapper}>
-        <DrawerLayoutAndroid
-          ref={drawerRef}
-          drawerPosition="left"
-          drawerWidth={250}
-          drawerLockMode="locked-open"
-          renderNavigationView={() => (
-            <View
-              style={{
-                flex: 1,
-                alignItems: "center",
-                justifyContent: "center",
-                backgroundColor: "purple",
-              }}
-            >
-              <Text>Oi Drawerzinho</Text>
-              <Button onPress={handleCloseMenu}>Fechar Menu</Button>
-            </View>
-          )}
-        >
-          <View style={styles.container}>
-            <Button onPress={handleOpenMenu}>Abrir Menu</Button>
-          </View>
-        </DrawerLayoutAndroid>
+        <View style={styles.container}>
+          <Input placeholder="Nome" inputAccessoryViewID="name" />
+          <Input placeholder="Email" inputAccessoryViewID="email" />
+          <Input placeholder="Idade" inputAccessoryViewID="age" />
+        </View>
+        <InputAccessoryView nativeID='name'>  {/* IOS only: InputAccessoryView is a view that is displayed above the keyboard */}
+          <Button>Accessory do Nome</Button>
+        </InputAccessoryView>
+        <InputAccessoryView nativeID='email'>
+          <Button>Accessory do Email</Button>
+        </InputAccessoryView>
+        <InputAccessoryView nativeID='age'>
+          <Button>Accessory da Idade</Button>
+        </InputAccessoryView>
       </SafeAreaView>
     </SafeAreaProvider>
   );
