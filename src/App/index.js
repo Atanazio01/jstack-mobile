c5b55c9de72241ec545fca18f75a75e2e983099a
Atanazio01 <marcosd.atanazio@gmail.com>
2026-06-17 19:38:31 -0300

    Default font in the app

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,8 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { styles } from "./styles";
+
+import { AppText } from "../components/AppText";
 
 
 export default function App() {
@@ -8,10 +10,8 @@ export default function App() {
     <SafeAreaProvider>
       <SafeAreaView style={styles.wrapper}>
         <View style={styles.container}>
-          <Text style={{
-            fontSize: 32,
-            fontFamily: "Danfo-Regular",
-          }}>Hello World</Text>
+          <AppText style={{ fontSize: 32 }}>Hello World</AppText>
+          <AppText style={{ fontSize: 16 }}>Hello JStack</AppText>
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
