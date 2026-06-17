942fac11ec81415cb9407d7e16f7684a60176247
Atanazio01 <marcosd.atanazio@gmail.com>
2026-06-17 19:58:04 -0300

    Making HTTP Requests

--- src/App/index.js
+++ src/App/index.js
@@ -1,17 +1,38 @@
-import { View } from "react-native";
+import { ActivityIndicator, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { styles } from "./styles";
 
+import { useEffect, useState } from "react";
 import { AppText } from "../components/AppText";
 
-
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos/1",
+      );
+      const json = await response.json();
+
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      
+      setData(json);
+      setIsLoading(false);
+    }
+    fetchData();
+  }, []);
+
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.wrapper}>
         <View style={styles.container}>
-          <AppText style={{ fontSize: 32 }}>Hello World</AppText>
-          <AppText style={{ fontSize: 16 }}>Hello JStack</AppText>
+          {isLoading ? (
+            <ActivityIndicator />
+          ) : (
+            <AppText style={{ fontSize: 32 }}>{data.title}</AppText>
+          )}
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
